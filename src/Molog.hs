88be59ade379ae71b8c9e74a4682113b7d1bcc43
Adam C. Foltzer <acfoltzer@gmail.com>
2013-03-02 22:30:15 -0800

    New architecture based around persistent-refs, and using slightly cleverer typeclasses for types involved in the logic system

--- src/Molog.hs
+++ src/Molog.hs
@@ -1,79 +1,160 @@
-{-# LANGUAGE EmptyDataDecls #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverlappingInstances #-}
-{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Molog where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Logic
-import "mtl" Control.Monad.Reader
-import "mtl" Control.Monad.State
+import Control.Monad.Ref
+import Control.Monad.ST.Persistent
 
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.STRef.Persistent
 
-import Unsafe.Coerce
+newtype Molog s a = M { unM :: STT s Logic a }
+  deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
 
-import Text.Printf
-import qualified Debug.Trace as DT
+instance MonadRef (STRef s) (Molog s) where
+  newRef       = M . newRef
+  readRef      = M . readRef
+  writeRef r x = M $ writeRef r x
 
-type VarId = Integer
+data Term s a = Var !(STRef s (Maybe (Term s a)))
+              | Val !a
+                deriving (Eq, Show)
 
-data LogicVal a = Var VarId | Val a
-                deriving (Eq)
+bindRef :: STRef s (Maybe (Term s a)) -> Term s a -> Molog s ()
+bindRef r t = writeRef r (Just t)
 
-instance Functor LogicVal where
-  fmap f (Var id) = Var id
-  fmap f (Val x)  = Val (f x)
+semiprune :: Term s a -> Molog s (Term s a)
+semiprune t@(Val _)   = return t
+semiprune t@(Var ref) = go t ref
+  where go t ref = do
+          mt <- readRef ref
+          case mt of
+            -- fresh var: done
+            Nothing            -> return t
+            -- one pointer away: done
+            Just (Val _)       -> return t
+            -- do path compression
+            Just t'@(Var ref') -> do
+              result <- go t' ref'
+              -- overwrite original pointer
+              bindRef ref result
+              return result
 
-instance Show a => Show (LogicVal a) where
-  show (Var id) = "_." ++ show id
-  show (Val x)  = show x
+fresh :: Molog s (Term s a)
+fresh = Var <$> newRef Nothing
 
-data WrappedVal
+class Unifiable s a where
+  unify :: Term s a -> Term s a -> Molog s ()
+  unifyWith :: (a -> a -> Molog s ()) -> Term s a -> Term s a -> Molog s ()
 
-instance Show WrappedVal where
-  show _ = "<val>"
+  default unify :: Eq a => Term s a -> Term s a -> Molog s ()
+  unify x y = unifyWith guardEq x y
+    where guardEq x y = guard (x == y)
 
-data SubstContents = SubstVar VarId 
-                   | SubstVal WrappedVal 
-                     deriving (Show)
+  unifyWith f x y = do
+    xt <- semiprune x
+    yt <- semiprune y
+    case (xt, yt) of
+      (Var xr, Var yr) | xr == yr  -> return ()
+                       | otherwise -> do
+        mxt' <- readRef xr
+        myt' <- readRef yr
+        case (mxt', myt') of
+          (Nothing, Nothing) -> bindRef xr yt
+          (Nothing, Just _ ) -> bindRef xr yt
+          (Just _ , Nothing) -> bindRef yr xt
+          (Just (Val xv), Just (Val yv)) -> f xv yv
+          _ -> error "impossible: more than 2 pointers after semiprune"
+      (Var xr, Val yv) -> do
+        mxt' <- readRef xr
+        case mxt' of
+          Nothing       -> bindRef xr yt
+          Just (Val xv) -> f xv yv
+          _ -> error "impossible: more than 2 pointers after semiprune"
+      (Val xv, Var yr) -> do
+        myt' <- readRef yr
+        case myt' of
+          Nothing       -> bindRef yr xt
+          Just (Val yv) -> f xv yv
+          _ -> error "impossible: more than 2 pointers after semiprune"
+      (Val xv, Val yv) -> f xv yv
 
-type Subst = Map VarId SubstContents
 
-emptyS :: Subst
-emptyS = Map.empty
+instance Unifiable s Int
 
-extendS :: LogicVal a -> LogicVal a -> Subst -> Subst
-extendS (Var id)  (Val val) s = Map.insert id (SubstVal (unsafeCoerce val)) s
-extendS (Var id1) (Var id2) s = Map.insert id1 (SubstVar id2) s
-extendS _         _         _ = error "cannot extend subst with a value"
 
-walk :: LogicVal a -> Subst -> LogicVal a
-walk (Val x) _ = Val x
-walk (Var id) s = case Map.lookup id s of
-                    Nothing -> Var id
-                    Just (SubstVar id') -> walk (Var id') s
-                    Just (SubstVal wrapped) -> Val (unsafeCoerce wrapped)
-            
-testx = Var 0 :: LogicVal Int
-testy = Var 1 :: LogicVal Int
+instance (Unifiable s a, Unifiable s b) => Unifiable s (Term s a, Term s b) where
+  unify x y = unifyWith unifyPairs x y
+    where --unifyPairs :: (Term s a, Term s b) -> (Term s a, Term s b) -> Molog s ()
+          unifyPairs (x1, y1) (x2, y2) = unify x1 x2 >> unify y1 y2
 
-tests = extendS testx testy emptyS
-tests' = extendS testy (Val 5) tests
 
-testWalk = walk testx tests'
+test :: Molog s (Term s Int)
+test = do x <- fresh
+          y <- fresh
+          x `unify` (Val 5)
+          x `unify` y
+          return y
 
-getSubst :: LogicComp Subst
-getSubst = snd <$> get
+test1 :: Molog s (Term s (Term s Int, Term s Int))
+test1 = do x <- fresh
+           y <- fresh
+           x `unify` (Val 5)
+           x `unify` y
+           p <- fresh
+           p `unify` (Val (x, y))
+           return p
 
-modifySubst :: (Subst -> Subst) -> LogicComp ()
-modifySubst f = modify (\(varId, s) -> (varId, f s))
+test2 :: Molog s (Term s Int)
+test2 = do x <- fresh
+           msum [ x `unify` (Val 5)
+                , x `unify` (Val 5) >> x `unify` (Val 6)
+                , x `unify` (Val 6)
+                ]
+           return x
+
+class Reifiable s a b | a -> b where
+  reify :: a -> Molog s (Maybe b)
+
+instance Reifiable s a b => Reifiable s (Term s a) b where
+  reify (Val v) = reify v
+  reify (Var ref) = do
+    mt <- readRef ref
+    case mt of
+      Just (Val v)    -> reify v -- return . maybe Nothing (Just . Val) =<< reify v
+      Just (Var ref') -> do
+        mt' <- readRef ref'
+        case mt' of
+          Just (Val v) -> reify v
+          _            -> return Nothing
+      _ -> return Nothing
+
+instance Reifiable s Int Int where
+  reify = return . Just
+
+instance (Reifiable s a c, Reifiable s b d) => Reifiable s (a, b) (c, d) where
+  reify (x, y) = do
+    mx' <- reify x
+    my' <- reify y
+    case (mx', my') of
+      (Just x', Just y') -> return (Just (x', y'))
+      _                  -> return Nothing
+
+runMolog :: (forall s . Molog s a) -> [a]
+runMolog c = observeAll (runSTT (unM c))
+
+{-
 
 class Eq a => Unifiable a where
   unify :: LogicVal a
@@ -165,8 +246,8 @@ testComp = do x <- var
 run :: Reifiable a => LogicComp (LogicVal a) -> [LogicVal a]
 run c = map reifyOne results
   where results = observeAll $ runStateT c emptyState
-        reifyOne (lv, (_, s)) = 
-          runReader (evalStateT (unRC $ reify lv) emptyRS) s          
+        reifyOne (lv, (_, s)) =
+          runReader (evalStateT (unRC $ reify lv) emptyRS) s
 
 testComp2 :: LogicComp (LogicVal Int)
 testComp2 = do x <- var
@@ -209,7 +290,7 @@ newtype ReifyComp a = RC { unRC :: StateT ReifySubst (Reader Subst) a }
 
 class Reifiable a where
   reify :: LogicVal a -> (ReifyComp (LogicVal a))
-  reify lv = do 
+  reify lv = do
     s <- ask
     case walk lv s of
       Val x -> return $ Val x
@@ -235,13 +316,14 @@ reifyVar (Var id) = do
   rs <- get
   case Map.lookup id rs of
     Just rname -> return $ Var rname
-    Nothing -> do 
+    Nothing -> do
       let rname = fromIntegral $ Map.size rs
       modify (Map.insert id rname)
       return $ Var rname
-reifyVar _ = error "reifyVar is only for fresh variables"                        
+reifyVar _ = error "reifyVar is only for fresh variables"
 
 --instance Reifiable a => Reifiable [a] where
 --  reify lv s = undefined
 
 -- instance Eq a => Reifiable (LogicVal a) where
+-}
